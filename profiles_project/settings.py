4dba7c4c48167e9bfbd5043887b38b18efeb211a
Shubham Thigale <Shubham.Thigale@harbingergroup.com>
2022-04-18 15:29:34 +0530

    Added custom user profile model,manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile' #we tell django to use our custom profile api for user registration or authentication
