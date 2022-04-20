6adacc76cd0e1e04e3966836f9ea6e9f82d90023
Shubham Thigale <Shubham.Thigale@harbingergroup.com>
2022-04-20 14:56:33 +0530

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):   #self is the parameter which is required by all class function, a request object is passed by django rest framework and contains details of request made to API, format is used to add format suffix to endpoint URL which we are not using in this case
+        """Returns a list of APIView features"""
+        an_apiview = [
+        'Name-Shubham',
+        'Org-HSPL',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
